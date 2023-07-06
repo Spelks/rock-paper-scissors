1e8c153edb78f7080d79bd2606f27f608f90b095
Alex <131786787+Spelks@users.noreply.github.com>
2023-07-06 12:49:37 +0100

    Add return to 'getComputerChoice()' function. Implement 'playRound()' function call.

--- script.js
+++ script.js
@@ -2,36 +2,37 @@ function getComputerChoice() {
     let rpsArray = ["rock", "paper", "scissors"];
     let rpsChoice = rpsArray[Math.floor(Math.random() * rpsArray.length)];
     if (rpsChoice === "rock") {
-        return "Computer picks 'Rock'!";
+        alert("Computer picks 'Rock'!");
     } else if (rpsChoice === "paper") {
-        return "Computer picks 'Paper'!";
+        alert("Computer picks 'Paper'!");
     } else {
-        return "Computer picks 'Scissors'!";
+        alert("Computer picks 'Scissors'!");
     }
+    return rpsChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
     if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose! 'Paper' beats 'Rock'!";
+        alert("You lose! 'Paper' beats 'Rock'!");
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! 'Paper' beats 'Rock'!";
+        alert("You win! 'Paper' beats 'Rock'!");
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose! 'Paper' beats 'Rock'!";
+        alert("You lose! 'Paper' beats 'Rock'!");
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! 'Paper' beats 'Rock'!";
+        alert("You win! 'Paper' beats 'Rock'!");
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose! 'Rock' beats 'Scissors'!";
+        alert("You lose! 'Rock' beats 'Scissors'!");
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! 'Rock' beats 'Scissors'!";
+        alert("You win! 'Rock' beats 'Scissors'!");
     } else if (playerSelection === computerSelection) {
-        return "It's a tie!"
+        alert("It's a tie!");
+    } else {
+        alert("Please select either 'Rock', 'Paper' or 'Scissors'!");
     }
 }
 
-const playerSelection = "rock";
+const playerSelection = prompt("'Rock', 'Paper' or 'Scissors'?").toLowerCase();
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+playRound(playerSelection, computerSelection);
 
-
-
+  
