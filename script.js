f2ca9a1ffd98e1668fa4e18099bda38cbaa9867d
Alex <131786787+Spelks@users.noreply.github.com>
2023-08-09 19:52:53 +0100

    Add query selectors and Event listerners to buttons.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     let rpsArray = ["ROCK", "PAPER", "SCISSORS"];
     let rpsChoice = rpsArray[Math.floor(Math.random() * rpsArray.length)];
-    alert (`Computer picks '${rpsChoice}!'`);
+    // alert (`Computer picks '${rpsChoice}!'`);
     return rpsChoice;
 }
 
@@ -11,7 +11,7 @@ function playRound(userChoice, computerChoice) {
       (userChoice === "PAPER" && computerChoice === "SCISSORS") ||
       (userChoice === "SCISSORS" && computerChoice === "ROCK") 
     )  {
-         alert(`You lose! '${computerChoice}' beats '${userChoice}'.`);
+        //  alert(`You lose! '${computerChoice}' beats '${userChoice}'.`);
          console.log("Point to Computer");
          computerScore++
          return computerScore;
@@ -20,33 +20,50 @@ function playRound(userChoice, computerChoice) {
       (userChoice === "SCISSORS" && computerChoice === "PAPER") ||
       (userChoice === "ROCK" && computerChoice === "SCISSORS") 
     ) {
-        alert(`You win! '${userChoice}' beats '${computerChoice}'.`);
+        // alert(`You win! '${userChoice}' beats '${computerChoice}'.`);
         console.log("Point to Player");
         userScore++
         return userScore;
     } else if (userChoice === computerChoice) 
       {
-        alert("It's a tie!");
+        // alert("It's a tie!");
+        console.log("It's a tie")
     } 
 }
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        userChoice = prompt("'Rock', 'Paper' or 'Scissors'?").toUpperCase();
+        userChoice = "Hello";
+        // userChoice = prompt("'Rock', 'Paper' or 'Scissors'?").toUpperCase();
         computerChoice = getComputerChoice();   
         playRound(userChoice, computerChoice)
     } if (userScore > computerScore) {
-        alert(`Computer: ${computerScore}. You: ${userScore}. YOU WIN!`);
+        // alert(`Computer: ${computerScore}. You: ${userScore}. YOU WIN!`);
     } else if (userScore < computerScore) {
-        alert(`Computer: ${computerScore}. You: ${userScore}. YOU LOSE!`);
+        // alert(`Computer: ${computerScore}. You: ${userScore}. YOU LOSE!`);
     } else if (userScore === computerScore) {
-        alert(`Computer: ${computerScore}. You: ${userScore}. IT'S A TIE!`);
+        // alert(`Computer: ${computerScore}. You: ${userScore}. IT'S A TIE!`);
     }
 }
+
+const rockBtn = document.querySelector(".rock-btn");
+rockBtn.addEventListener("click", () => {
+    playRound(userChoice = "ROCK", computerChoice);
+    console.log(userChoice);
+});
+
+const paperBtn = document.querySelector(".paper-btn");
+paperBtn.addEventListener("click", () => {
+    playRound(userChoice = "PAPER", computerChoice);
+    console.log(userChoice)
+})
+
+const scissorsBtn = document.querySelector(".scissors-btn");
+scissorsBtn.addEventListener("click", () => {
+    playRound(userChoice = "SCISSORS", computerChoice);
+    console.log(userChoice)
+})
 
 let userScore = 0;
 let computerScore = 0;
 game()
-
-
-  
