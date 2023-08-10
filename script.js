f66c0558e3fb8eaf0c8b6e0776ec1d7e7fa04516
Alex <131786787+Spelks@users.noreply.github.com>
2023-08-10 12:22:03 +0100

    Add styling. Modify 'score-container'. Add JS code comments. Fix score system.

--- script.js
+++ script.js
@@ -1,67 +1,71 @@
+//Randomly picks the Computer choice.
 function getComputerChoice() {
     let rpsArray = ["ROCK", "PAPER", "SCISSORS"];
     let rpsChoice = rpsArray[Math.floor(Math.random() * rpsArray.length)];
-    // alert (`Computer picks '${rpsChoice}!'`);
     return rpsChoice;
 }
 
+//Takes the player choice and compares it to the random Computer choice.
 function playRound(userChoice, computerChoice) { 
     if (
       (userChoice === "ROCK" && computerChoice === "PAPER") ||
       (userChoice === "PAPER" && computerChoice === "SCISSORS") ||
       (userChoice === "SCISSORS" && computerChoice === "ROCK") 
-    )  {
-        //  alert(`You lose! '${computerChoice}' beats '${userChoice}'.`);
-         console.log("Point to Computer");
+    )  { //Displays message for round loss and assigns the Computer a point
+         let cpuPoint = document.querySelector(".score-message h3");
+         cpuPoint.textContent = `COMPUTER GETS A POINT! \nComputer picked '${computerChoice}'. \nYou picked '${userChoice}' !`;
          computerScore++
+         let pointToCpu = document.querySelector(".cpu-score h3 span")
+         pointToCpu.textContent = `${computerScore}`
          return computerScore;
     } else if (
       (userChoice === "PAPER" && computerChoice === "ROCK") ||
       (userChoice === "SCISSORS" && computerChoice === "PAPER") ||
       (userChoice === "ROCK" && computerChoice === "SCISSORS") 
-    ) {
-        // alert(`You win! '${userChoice}' beats '${computerChoice}'.`);
-        console.log("Point to Player");
-        userScore++
+    ) { //Displays message for round win and assigns the Player a point
+         let playerPoint = document.querySelector(".score-message h3");
+         playerPoint.textContent = `YOU GET A POINT! \nComputer picked '${computerChoice}'. \nYou picked '${userChoice}' !`;
+         userScore++
+         let pointToPlayer = document.querySelector(".player-score h3 span")
+         pointToPlayer.textContent = `${userScore}`
         return userScore;
     } else if (userChoice === computerChoice) 
-      {
-        // alert("It's a tie!");
-        console.log("It's a tie")
+      { //Displays message for a tie round and does not add point to either
+        let tiePoint = document.querySelector(".score-message h3");
+        tiePoint.textContent = `NO POINTS! \nComputer picked '${computerChoice}'. \nYou picked '${userChoice}' !`;
     } 
 }
 
+// Game function to loop a single game through 5 rounds before declaring a winner
 function game() {
     for (let i = 0; i < 5; i++) {
-        userChoice = "Hello";
-        // userChoice = prompt("'Rock', 'Paper' or 'Scissors'?").toUpperCase();
+        userChoice = "";
         computerChoice = getComputerChoice();   
         playRound(userChoice, computerChoice)
     } if (userScore > computerScore) {
-        // alert(`Computer: ${computerScore}. You: ${userScore}. YOU WIN!`);
+        let playerWins = document.querySelector(".score-message h3");
+        playerWins.textContent = "Congratulations! \nYou beat the Computer.";
     } else if (userScore < computerScore) {
-        // alert(`Computer: ${computerScore}. You: ${userScore}. YOU LOSE!`);
+        let cpuWins = document.querySelector(".score-message h3");
+        cpuWins.textContent ="Computer wins! \nTry again?";
     } else if (userScore === computerScore) {
-        // alert(`Computer: ${computerScore}. You: ${userScore}. IT'S A TIE!`);
+        let noWins = document.querySelector(".score-message h3");
+        noWins.textContent ="Game ends in a draw!";
     }
 }
 
+// Player button functionality
 const rockBtn = document.querySelector(".rock-btn");
 rockBtn.addEventListener("click", () => {
-    playRound(userChoice = "ROCK", computerChoice);
-    console.log(userChoice);
+    playRound(userChoice = "ROCK", computerChoice = getComputerChoice());
 });
-
 const paperBtn = document.querySelector(".paper-btn");
 paperBtn.addEventListener("click", () => {
-    playRound(userChoice = "PAPER", computerChoice);
-    console.log(userChoice)
+    playRound(userChoice = "PAPER", computerChoice = getComputerChoice());
 })
-
 const scissorsBtn = document.querySelector(".scissors-btn");
 scissorsBtn.addEventListener("click", () => {
-    playRound(userChoice = "SCISSORS", computerChoice);
-    console.log(userChoice)
+    playRound(userChoice = "SCISSORS", computerChoice = getComputerChoice());
 })
 
 let userScore = 0;
