ae2238411562ef5e88626f854a23cb818059fdac
Alex <131786787+Spelks@users.noreply.github.com>
2023-08-12 10:33:44 +0100

    Add game reset button upon completion.

--- script.js
+++ script.js
@@ -5,48 +5,61 @@ function getComputerChoice() {
     return rpsChoice;
 }
 
+//Disable buttons once Player/Computer reaches 3 points
+function btnDisable() {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+}
+
+function myFunction() {
+    const centerData = document.querySelector(".reset-button");
+    const resetBtn = document.createElement("BUTTON");
+    const resetTxt = document.createTextNode("Play Again?");
+    resetBtn.appendChild(resetTxt);
+    centerData.appendChild(resetBtn);
+    // document.body.appendChild(resetBtn);
+    resetBtn.addEventListener("click", () => location.reload());
+  }
+
 function playRound(userChoice, computerChoice) {
-    if (userScore === 3) {
-        playerWins = document.querySelector(".score-message h3");
-        playerWins.textContent = `Congratulations! You Win!`;
-    } else if (computerScore === 3) {
-        cpuWins = document.querySelector(".score-message h3");
-        cpuWins.textContent = `Computer Wins!`;
-    } else {
-        if (
-            (userChoice === "ROCK" && computerChoice === "PAPER") ||
-            (userChoice === "PAPER" && computerChoice === "SCISSORS") ||
-            (userChoice === "SCISSORS" && computerChoice === "ROCK")
-            )  { //Displays message for round loss and assigns the Computer a point
-                let cpuPoint = document.querySelector(".score-message h3");
-                cpuPoint.textContent = `'${userChoice}' vs '${computerChoice}'. \nComputer gets a Point!`;
-                computerScore++
-                let pointToCpu = document.querySelector(".cpu-score h3 span")
-                pointToCpu.textContent = `${computerScore}`
-                    if (computerScore === 3) {
-                        let cpuWins = document.querySelector(".score-message h3");
-                        cpuWins.textContent = `'${userChoice}' loses to '${computerChoice}' \nComputer Wins!`;
-                    }
-            } else if (
-            (userChoice === "PAPER" && computerChoice === "ROCK") ||
-            (userChoice === "SCISSORS" && computerChoice === "PAPER") ||
-            (userChoice === "ROCK" && computerChoice === "SCISSORS") 
-            ) { //Displays message for round win and assigns the Player a point
-                let playerPoint = document.querySelector(".score-message h3");
-                playerPoint.textContent = `'${userChoice}' vs '${computerChoice}'. \nPlayer gets a Point!`;
-                userScore++
-                let pointToPlayer = document.querySelector(".player-score h3 span")
-                pointToPlayer.textContent = `${userScore}`
-                    if (userScore === 3) {
-                        let playerWins = document.querySelector(".score-message h3");
-                        playerWins.textContent = `'${userChoice}' beats '${computerChoice}' \nCongratulations! You Win!`;
-                    }
-            } else if (userChoice === computerChoice) 
-            { //Displays message for a tie round and does not add point to either
-                let tiePoint = document.querySelector(".score-message h3");
-                tiePoint.textContent = `'${userChoice}' vs '${computerChoice}'. \nNobody gets a Point!`;
-            } 
-    }
+    if (
+        (userChoice === "ROCK" && computerChoice === "PAPER") ||
+        (userChoice === "PAPER" && computerChoice === "SCISSORS") ||
+        (userChoice === "SCISSORS" && computerChoice === "ROCK")
+        )  { //Displays message for round loss and assigns the Computer a point
+            let cpuPoint = document.querySelector(".score-message h3");
+            cpuPoint.textContent = `'${userChoice}' vs '${computerChoice}'. \nComputer gets a Point!`;
+            computerScore++
+            let pointToCpu = document.querySelector(".cpu-score h3 span")
+            pointToCpu.textContent = `${computerScore}`
+                if (computerScore === 3) {
+                    let cpuWins = document.querySelector(".score-message h3");
+                    cpuWins.textContent = `'${userChoice}' loses to '${computerChoice}' \nComputer Wins!`;
+                    btnDisable()
+                    myFunction()
+                }
+        } else if (
+        (userChoice === "PAPER" && computerChoice === "ROCK") ||
+        (userChoice === "SCISSORS" && computerChoice === "PAPER") ||
+        (userChoice === "ROCK" && computerChoice === "SCISSORS") 
+        ) { //Displays message for round win and assigns the Player a point
+            let playerPoint = document.querySelector(".score-message h3");
+            playerPoint.textContent = `'${userChoice}' vs '${computerChoice}'. \nPlayer gets a Point!`;
+            userScore++
+            let pointToPlayer = document.querySelector(".player-score h3 span")
+            pointToPlayer.textContent = `${userScore}`
+                if (userScore === 3) {
+                    let playerWins = document.querySelector(".score-message h3");
+                    playerWins.textContent = `'${userChoice}' beats '${computerChoice}' \nCongratulations! You Win!`;
+                    btnDisable()
+                    myFunction()
+                }
+        } else if (userChoice === computerChoice) 
+        { //Displays message for a tie round and does not add point to either
+            let tiePoint = document.querySelector(".score-message h3");
+            tiePoint.textContent = `'${userChoice}' vs '${computerChoice}'. \nNobody gets a Point!`;
+        } 
 }
 
 // Player button functionality
