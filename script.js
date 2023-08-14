ebccb6a8b09fa1a429e47f5f108805c7303ebc62
Alex <131786787+Spelks@users.noreply.github.com>
2023-08-14 20:21:37 +0100

    Add additional styling. Add highlight to Computer button choices on each round.

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-//Randomly picks the Computer choice.
+//Randomly picks the Computer choice and highlights it in UI.
 function getComputerChoice() {
     let rpsArray = ["ROCK", "PAPER", "SCISSORS"];
     let rpsChoice = rpsArray[Math.floor(Math.random() * rpsArray.length)];
+    const rpsRock = document.querySelector(".rock-cpu");
+    const rpsPaper = document.querySelector(".paper-cpu");
+    const rpsScissors = document.querySelector(".scissors-cpu");
+    if (rpsChoice === "ROCK") {
+        rpsRock.style.cssText = 
+        "background-color: grey; box-shadow: 0 0 50px 15px rgb(255, 45, 45);";
+        rpsPaper.style.cssText = null;
+        rpsScissors.style.cssText = null;
+    } else if (rpsChoice === "PAPER") {
+        rpsRock.style.cssText = null
+        rpsPaper.style.cssText =
+        "background-color: grey; box-shadow: 0 0 50px 15px rgb(255, 45, 45);";
+        rpsScissors.style.cssText = null;
+    } else if (rpsChoice === "SCISSORS") {
+        rpsRock.style.cssText = null;
+        rpsPaper.style.cssText = null;
+        rpsScissors.style.cssText =
+        "background-color: grey; box-shadow: 0 0 50px 15px rgb(255, 45, 45);";
+    }
     return rpsChoice;
 }
 
@@ -12,16 +31,20 @@ function btnDisable() {
     scissorsBtn.disabled = true;
 }
 
-function myFunction() {
+//Reset button that appears once the game reaches the end
+function btnReset() {
     const centerData = document.querySelector(".reset-button");
     const resetBtn = document.createElement("BUTTON");
+    resetBtn.style.cssText = 
+    "border: 10px solid #00FF6C; padding: 10px 20px; border-radius: 30px;" + 
+    "font-size: 2rem; color: #fff; background-color: transparent; cursor: pointer;";
     const resetTxt = document.createTextNode("Play Again?");
     resetBtn.appendChild(resetTxt);
     centerData.appendChild(resetBtn);
-    // document.body.appendChild(resetBtn);
     resetBtn.addEventListener("click", () => location.reload());
   }
 
+  //Main game code. Matches button event lister clicks to getComputerChoice function and rounds up to end of game
 function playRound(userChoice, computerChoice) {
     if (
         (userChoice === "ROCK" && computerChoice === "PAPER") ||
@@ -37,7 +60,7 @@ function playRound(userChoice, computerChoice) {
                     let cpuWins = document.querySelector(".score-message h3");
                     cpuWins.textContent = `'${userChoice}' loses to '${computerChoice}' \nComputer Wins!`;
                     btnDisable()
-                    myFunction()
+                    btnReset()
                 }
         } else if (
         (userChoice === "PAPER" && computerChoice === "ROCK") ||
@@ -53,28 +76,51 @@ function playRound(userChoice, computerChoice) {
                     let playerWins = document.querySelector(".score-message h3");
                     playerWins.textContent = `'${userChoice}' beats '${computerChoice}' \nCongratulations! You Win!`;
                     btnDisable()
-                    myFunction()
+                    btnReset()
                 }
         } else if (userChoice === computerChoice) 
         { //Displays message for a tie round and does not add point to either
             let tiePoint = document.querySelector(".score-message h3");
             tiePoint.textContent = `'${userChoice}' vs '${computerChoice}'. \nNobody gets a Point!`;
-        } 
+        }
+    }
+
+function userUI(userChoice) {
+    const userRock = document.querySelector(".rock-btn");
+    const userPaper = document.querySelector(".paper-btn");
+    const userScissors = document.querySelector(".scissors-btn");
+    if (userChoice === "ROCK") {
+        userRock.style.cssText = "background-color: grey; box-shadow: 0 0 50px 15px #00FF6C;";
+        userPaper.style.cssText = null;
+        userScissors.style.cssText = null;
+    } else if (userChoice === "PAPER") {
+        userRock.style.cssText = null;
+        userPaper.style.cssText = "background-color: grey; box-shadow: 0 0 50px 15px #00FF6C;";
+        userScissors.style.cssText = null;
+    } else if (userChoice === "SCISSORS") {
+        userRock.style.cssText = null;
+        userPaper.style.cssText = null;
+        userScissors.style.cssText = "background-color: grey; box-shadow: 0 0 50px 15px #00FF6C;";
+    }
 }
 
 // Player button functionality
 const rockBtn = document.querySelector(".rock-btn");
 rockBtn.addEventListener("click", () => {
     playRound(userChoice = "ROCK", computerChoice = getComputerChoice());
+    userUI(userChoice);
 });
 const paperBtn = document.querySelector(".paper-btn");
 paperBtn.addEventListener("click", () => {
     playRound(userChoice = "PAPER", computerChoice = getComputerChoice());
+    userUI(userChoice);
 })
 const scissorsBtn = document.querySelector(".scissors-btn");
 scissorsBtn.addEventListener("click", () => {
     playRound(userChoice = "SCISSORS", computerChoice = getComputerChoice());
+    userUI(userChoice);
 })
 
+//Global variables to ensure blank slate at start of game
 let userScore = 0;
 let computerScore = 0;
